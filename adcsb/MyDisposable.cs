afa82576e74e47186e3d9261aa6a2682e715ceee
Manuel Riezebosch <mriezebosch@gmail.com>
2014-09-22 09:51:11 +0200

    object initializer

---
+++ adcsb/MyDisposable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace adcsb
+{
+    class MyDisposable : IDisposable
+    {
+        public int Leeftijd { get; set; }
+    }
+}
--- adcsb/UnitTest1.cs
+++ adcsb/UnitTest1.cs
@@ -12,5 +12,29 @@ namespace adcsb
             var p = new Persoon();
             Assert.AreEqual(12, p.Leeftijd);
         }
+
+        [TestMethod]
+        public void ObjectInitializers()
+        {
+            var p = new Persoon();
+            p.Naam = "Manuel Riezebosch";
+            p.Leeftijd = 32;
+            PrintPersoon(p);
+
+            PrintPersoon(new Persoon { 
+                Naam = "Manuel Riezebosch", 
+                Leeftijd = 32 
+            });
+
+            using (var myDisposable = new MyDisposable { Leeftijd = 3})
+            {
+
+            }
+        }
+
+        private void PrintPersoon(Persoon persoon)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
