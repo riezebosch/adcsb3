19ef89da85efc1ea19dffabbb6c7bb9e7a10a933
Manuel Riezebosch <mriezebosch@gmail.com>
2014-09-22 09:34:29 +0200

    persoon vergeten

---
+++ adcsb/Persoon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adcsb
+{
+    partial class Persoon
+    {
+        public int Leeftijd { get; set; }
+
+        partial void CustomInitialize(int p)
+        {
+            Leeftijd = p;
+        }
+    }
+}
