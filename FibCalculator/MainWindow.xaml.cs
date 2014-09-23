cbd412af51de9ea3bdf4df25c65c4e72da2407e2
Manuel Riezebosch <mriezebosch@gmail.com>
2014-09-23 13:09:33 +0200

    Progress op fibonacci

--- FibCalculator/MainWindow.xaml.cs
+++ FibCalculator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,21 +26,56 @@ namespace FibCalculator
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        CancellationTokenSource _tokenSource;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
             var input = int.Parse(Input.Text);
-            Task.Run(() => Fib(input))
-                .ContinueWith(t => Result.Content = t.Result, TaskScheduler.FromCurrentSynchronizationContext());
+
+            _tokenSource = new CancellationTokenSource();
+            try
+            {
+                int nMax = 0;
+                var progress = new Progress<int>(n => ProgressLabel.Content = n);
+                Result.Content = await Task.Run(() => Fib(input, _tokenSource.Token, progress, true));
+            }
+            catch (OperationCanceledException)
+            {
+            }
         }
 
-        private int Fib(int n)
+        private static int Fib(int n,
+            CancellationToken token,
+            IProgress<int> progress, bool report)
         {
+
+            token.ThrowIfCancellationRequested();
+
             if (n <= 1)
             {
                 return n;
             }
+            
+            checked
+            {
+                int result = Fib(n - 1, token, progress, report) +
+                    Fib(n - 2, token, progress, false);
 
-            return Fib(n - 1) + Fib(n - 2);
+                if (report)
+                {
+                    progress.Report(n);
+                }
+
+                return result;
+            }
         }
-      }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (_tokenSource != null)
+            {
+                _tokenSource.Cancel();
+            }
+        }
+    }
 }
